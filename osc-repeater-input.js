6309f87c1e9900af1857aad9c7753df7b1a68de5
Louis McCallum <louisbusby@yahoo.com>
2020-06-09 12:15:57 +0100

    input and output

---
+++ osc-repeater-input.js
@@ -0,0 +1,31 @@
+const WebSocket = require('ws');
+const { Server } = require('node-osc');
+const INPUT_PORT = 12345;
+const OSC_ADDRESS = "/toMimic"
+var oscServer = new Server(INPUT_PORT, '127.0.0.1', () => {
+  console.log('OSC Server is listening');
+});
+
+oscServer.on('message', function (msg) {
+  console.log(msg);
+  let floats  = [];
+  msg.forEach((v)=> {
+    let f = parseFloat(v);
+    if(!isNaN(f))
+    {
+      floats.push(f);
+    }
+  });
+  console.log('sending', floats);
+  if(socket !== undefined)
+  {
+    socket.send(JSON.stringify(floats))
+  }
+});
+
+const wss = new WebSocket.Server({port: 8080});
+let socket;
+console.log("Running repeater");
+wss.on('connection', function connection(ws) {
+  socket = ws;
+})
